4b430b44ab3a7db2f6f879a7ca302ce1e5129b65
Tanmaya Murthy <90868676+A00455084@users.noreply.github.com>
2022-04-13 18:21:16 -0300

    Add slider and radio button input controls

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,16 @@ import numpy as np
 
 st.title('Bitcoin Prices')
 
+# slider input for no of days
+days = st.slider('No. of days', 0, 365, 90)
+
+# radio button input for currency
+currency = st.radio(
+     "Currency",
+     ('cad', 'usd', 'inr'))
+
+# endpoint
+
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 
 payload={'vs_currency':'cad','days':90,'interval':'daily'}
@@ -19,4 +29,6 @@ data_df = pd.DataFrame(data=data['prices'],columns=['Date','Price'])
 print(data_df.head())
 data_df['Date'] = pd.to_datetime(data_df['Date'],unit='ms',origin='unix')
 data_df.set_index('Date',inplace=True)
+
+# plot line chart
 st.line_chart(data_df)
