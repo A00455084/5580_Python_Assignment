71ff70cd9a1f73e02f76a5716c4075f90dbc949f
Tanmaya Murthy <90868676+A00455084@users.noreply.github.com>
2022-04-16 00:19:49 -0300

    Code cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,30 +14,24 @@ currency = st.radio(
      ('cad', 'usd', 'inr'))
 
 # endpoint
-
 url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
-
 payload={'vs_currency':currency,'days':days,'interval':'daily'}
 headers = {}
-
 response = requests.request("GET", url, headers=headers, params=payload)
 if response.status_code==200:
     data = response.json()
+    # prepare dataframe
+    data_df = pd.DataFrame(data=data['prices'],columns=['Date','Price'])
+    print(data_df.head())
+    data_df['Date'] = pd.to_datetime(data_df['Date'],unit='ms',origin='unix')
+    data_df.set_index('Date',inplace=True)
+    # calculate avg price over time frame
+    avg_price = data_df['Price'].mean()
+    # for custom legend name according to currency selected
+    data_df.rename(columns={'Price': currency.upper()},inplace=True)
+    # plot line chart
+    st.line_chart(data_df)
+    st.write("Average price during this time was ",str(avg_price)," ",currency)
 
-
-data_df = pd.DataFrame(data=data['prices'],columns=['Date','Price'])
-print(data_df.head())
-data_df['Date'] = pd.to_datetime(data_df['Date'],unit='ms',origin='unix')
-data_df.set_index('Date',inplace=True)
-
-
-# calculate avg price over time frame
-avg_price = data_df['Price'].mean()
-
-# for custom legend name according to currency selected
-data_df.rename(columns={'Price': currency.upper()},inplace=True)
-
-# plot line chart
-st.line_chart(data_df)
-
-st.write("Average price during this time was ",str(avg_price)," ",currency)
+else:
+    st.write("Request failed with error code: ",str(response.status_code))
